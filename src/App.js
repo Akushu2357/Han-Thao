627919ebd7f9e4322be50c21a5b500fe128ff42f
Akushu <tang.tang2004@gmail.com>
2024-10-17 19:05:52 +0700

    update

--- src/App.js
+++ src/App.js
@@ -2,6 +2,32 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const [people, setPeople] = useState([]);
+  const [menu, setMenu] = useState([]);
+  let id_menu = 0;
+  let id_people = 0;
+
+  const add_people = () => {
+    setPeople([...people, {'id':id_people, 'name':''}])
+    id_people += 1;
+  };
+  const remove_people = (e) => {
+    const name = e.target.value;
+    setPeople(people.filter((e) => (e['name'] !== name)))
+  };
+
+  const add_menu = () => {
+    setMenu([...menu, {'id':id_menu, 'name':"", "price":0}])
+    id_menu += 1;
+  };
+  const rmove_menu = (e) => {
+    const name = e.target.value;
+    setMenu(menu.filter((e) => (e['name'] !== name)))
+  };
+
+  const cal_per_person_per_menu = () => {
+
+  };
   return (
     <div className="App">
       <header className="App-header">
